6a90ab2cd16b9a0831af270562a218da4c9ad33d
IgorChalenko <nobu598@gmail.com>
2021-05-06 22:06:14 +0300

    created rails api with main functional

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'api/v1/users', to: 'users#index'
+  get 'api/v1/users/:id', to: 'users#show', as: 'user'
+  post 'api/v1/users', to: 'users#create'
+  put 'api/v1/users/:id', to:'users#update'
+  delete 'api/v1/users/:id', to: 'users#destroy'
+end
