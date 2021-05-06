85589052198823049fab1258cf00485b9b42229c
IgorChalenko <nobu598@gmail.com>
2021-05-06 22:49:22 +0300

    "Add regexp to email validation"

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_05_06_191647) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.integer "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
