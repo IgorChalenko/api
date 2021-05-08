d503393be557c1eaef9e3b323f015d4810bd9e38
IgorChalenko <nobu598@gmail.com>
2021-05-08 20:16:02 +0300

    Edit routes namespace

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,96 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        @users = User.all
+        render json: { 'success': true,
+                      'message': 'Founded data',
+                      'data': {
+                        'users': @users
+                      } }
+      end
+
+      def show
+        @user = User.find(params[:id])
+        render json: { 'success': true,
+                      'message': 'User founded',
+                      'data': {
+                        'user': {
+                          'id': @user.id,
+                          'name': @user.name,
+                          'email': @user.email,
+                          'password': @user.password_digest,
+                          'created_at': @user.created_at,
+                          'updated_at': @user.updated_at
+                        }
+                      } }
+      end
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: { 'success': true,
+                        'message': 'User created',
+                        'data': {
+                          'user': {
+                            'id': @user.id,
+                            'name': @user.name,
+                            'email': @user.email,
+                            'password': @user.password_digest,
+                            'created_at': @user.created_at,
+                            'updated_at': @user.updated_at
+                          }
+                        } }
+        else
+          render json: {  "success": false,
+                          "message": 'Invalid email or password',
+                          "data": {} }
+        end
+      end
+
+      def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        if @user.save
+          render json: { 'success': true,
+                        'message': 'User updated',
+                        'data': {
+                          'user': {
+                            'id': @user.id,
+                            'name': @user.name,
+                            'email': @user.email,
+                            'password': @user.password_digest,
+                            'created_at': @user.created_at,
+                            'updated_at': @user.updated_at
+                          }
+                        } }
+        else
+          render json: { 'success': false,
+                        'message': 'Something goes wrong' }
+        end
+      end
+
+      def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        render json: { 'success': true,
+                      'message': 'User seccesfully deleted' }
+      end
+
+      def login
+        @user = User.find_by_email(params[:email]).authenticate(params[:password])
+        if @user
+          render json: { 'status': 'sucsess' }
+        else
+          render json: { 'errored': 'error' }
+        end
+      end
+
+      private
+
+      def user_params
+        params.permit(:name, :email, :password)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'api/v1/users', to: 'users#index'
-  get 'api/v1/users/:id', to: 'users#show', as: 'user'
-  post 'api/v1/users', to: 'users#create'
-  put 'api/v1/users/:id', to: 'users#update'
-  delete 'api/v1/users/:id', to: 'users#destroy'
-  post 'api/v1/users/login', to: 'users#login'
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      post 'users/login', to: 'users#login'
+    end
+  end
 end
